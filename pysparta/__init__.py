592e331e50eb4ebf939a6e3d02c14405cf0ba05c
Jose A Ruiz Arias <jose.ruiz-arias@solargis.com>
2023-06-12 21:01:09 +0200

    Added merra2 local databases support

--- pysparta/__init__.py
+++ pysparta/__init__.py
@@ -1,5 +1,15 @@
+# flake8: noqa
 
 from .version import version as __version__  # pylint: disable=import-error
 
 from .models.sparta import SPARTA
 from . import benchmark_models, data
+
+
+# def calculate_clearsky(times, lats, lons, model_name='SPARTA', database='merra2_lta'):
+#     import sunwhere
+
+#     sp = sunwhere.sites(times, latitude=lats, longitude=lons)
+#     model_ = ... # get model function
+#     atmos = ... # search database
+#     # perform simulation, and return DataFrame
--- pysparta/data/__init__.py
+++ pysparta/data/__init__.py
@@ -1,17 +1,17 @@
 # flake8: noqa
+# pylint: disable=no-name-in-module
+
+import os
+import importlib
 
 from .io import save, load
-from .merra2 import get_clearsky_atmosphere as get_merra2_clearsky_atmosphere
+from .merra2_local import LTADataset as _LTADataset
+from .merra2_local import DailyDatasets as _DailyDatasets
+from .merra2_remote import get_clearsky_atmosphere as get_merra2_clearsky_atmosphere
 
-# from pathlib import Path
-# import pandas as pd
-# import pysparta
-# pysparta.data.save('afile.csv', **atmos)
-# atmos = pysparta.data.load('afile.csv')
+merra2_lta = _LTADataset(importlib.resources.files('pysparta.data') / 'merra2_lta')
 
-# import sunwhere
-# sp = sunwhere.sites(atmos['data'].index, longitude=atmos['gridcell_lon'],
-#                     latitude=atmos['gridcell_lat'])
-# pd.DataFrame(index=atmos['data'].index,
-#              data=pysparta.SPARTA(
-#                  cosz=sp.cosz, ecf=sp.ecf, **atmos['data'], as_dict=True)).plot()
+def merra2_daily(path=None):
+    if not (data_path := path or os.environ.get('MERRA2_DAILY_PATH', None)):
+        raise ValueError('missing path to MERRA-2 daily data')
+    return _DailyDatasets(data_path)
