7855df16c0526055a5457ce1a692c90c478041cb
Jose A Ruiz Arias <jose.ruiz-arias@solargis.com>
2026-04-27 07:28:30 +0200

    clean included in Makefile

--- clean.sh
+++
@@ -1,8 +0,0 @@
-rm -rf build
-rm -rf dist
-rm -rf *.egg-info
-rm -rf pysparta/version.py
-rm -rf pysparta.egg-info
-rm -rf .pytest_cache
-rm -f MANIFEST
-find . -name "__pycache__" -print0 | xargs -0 -I {} /bin/rm -rf "{}"
